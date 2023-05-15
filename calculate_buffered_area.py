a647dd2e56743f2455e97193da5801276a4111df
gwarren-nv5 <107498819+gwarren-nv5@users.noreply.github.com>
2023-05-15 15:10:06 -0700

    Update calculate_buffered_area.py
    add more unit types, conversions. Additional, optional variable is 'units' which will be returned at the end of the function

--- calculate_buffered_area.py
+++ calculate_buffered_area.py
@@ -4,7 +4,7 @@
 import geopandas as gpd
 from shapely.geometry import Polygon, MultiPolygon
 
-def calculate_polygon_area(shapefile):
+def calculate_polygon_area(shapefile, units="square miles"):
     # Read the shapefile
     gdf = gpd.read_file(shapefile)
 
@@ -18,9 +18,21 @@ def calculate_polygon_area(shapefile):
     # Calculate the area of the polygons (in square meters)
     gdf["area"] = gdf["geometry"].area
 
-    # Convert the area to square miles (1 square meter = 3.86102e-7 square miles)
-    gdf["area"] = gdf["area"] * 3.86102e-7
+    # Convert the area -- default to sq miles
+    if units == "square miles":
+        gdf["area"] = gdf["area"] * 3.86102e-7
+    elif units == "square kilometers":
+        gdf["area"] = gdf["area"] * 1e-6
+    elif units == "square meters":
+        pass
+    elif units == "square feet":
+        gdf["area"] = gdf["area"] * 10.7639
+    elif units == "acres":
+        gdf["area"] = gdf["area"] * 0.000247105
+    else:
+        raise ValueError("The units must be one of the following: square miles, square kilometers, square meters, square feet, or acres")
 
     # Sum and return the total area, rounded to 3 digits
     total_area = gdf["area"].sum()
     return round(total_area, 3)
+    return units
